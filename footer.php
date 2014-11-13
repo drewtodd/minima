7633cea7646731659727f285c962584072c45a71
Andrew Todd <drew.todd@gmail.com>
2014-11-12 16:32:46 -0800

    Break template into initial WordPress file structure...
    Signed-off-by: Andrew Todd <drew.todd@gmail.com>

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+
+            <div id="footer">
+                <p>Copyright and whatnot...</p>
+            </div>  
+        </div>
+    
+        <?php wp_footer(); ?>
+    </body>
+    </html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title></title>
+            <!--[if lt IE 9]>
+                <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+            <link rel="stylesheet" media="all" href="<?php bloginfo('stylesheet_url');?>">
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
+        
+        <?php wp_head(); ?>
+    </head>
+
+    <body lang="en">
+        <div id="page">
+            <div id="header">
+                <div id="navmain">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Archive</a></li>
+                    </ul>
+                </div>
+                <h1 class="sitehead"><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></h1>
+            </div>
+
