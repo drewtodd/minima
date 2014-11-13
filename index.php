4906a135917d068b84ed175777a576d11f34fd2b
Andrew Todd <drew.todd@gmail.com>
2014-11-12 17:37:55 -0800

    Link fix
    Signed-off-by: Andrew Todd <drew.todd@gmail.com>

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 			<div id="content">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-					<h1><a href="<?php get_permalink(); ?>"><?php the_title(); ?></a></h1>	
+					<h1><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h1>	
 					<?php the_content(); ?>
 
 				<?php endwhile; else: ?>
