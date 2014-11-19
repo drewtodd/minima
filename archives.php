85040d6afc4a8262745bbe24d25b1ded9b7b64eb
Andrew Todd <drew.todd@gmail.com>
2014-11-19 14:57:41 -0800

    Working archives.php with Title and Excerpts...
    Signed-off-by: Andrew Todd <drew.todd@gmail.com>

--- archives.php
+++ archives.php
@@ -19,15 +19,23 @@ get_header(); ?>
                             </ul>-->
                         </article>
     
-                        <?php endwhile; ?>
+                        <?php endwhile; wp_reset_postdata(); ?>
 
                         <!-- new loop -->
 
-                        <?php $posts = new WP_Query( array ( 'orderby' => 'date', 'order' => 'DESC') ); ?>
+                        <?php $posts = new WP_Query( array ( 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => 10) ); ?>
                         <?php if ( $posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
-                        <?php echo '<h3>' . get_the_title() . '</h3>'; ?>
-                        <?php echo '<p>' . get_the_excerpt() . '</h3>'; ?>
+                        <?php echo '<h3 id="' . $post->post_name . '"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>'; ?>
+                        <?php echo '<p>' . get_the_excerpt() . '</p>'; ?>
                         <?php endwhile; endif; wp_reset_postdata(); ?>
+
+                        <nav>
+                            <div class="navpage">
+                                 <div class="nav-prev"><?php previous_posts_link(); ?></div>
+                                 <div class="nav-next"><?php next_posts_link(); ?></div>
+                           </div>
+                        </nav>
+
 
                     <?php else: ?>
                         <p><?php _e('Sorry, this page does not exist.'); ?></p>
